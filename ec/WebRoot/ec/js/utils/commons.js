0ad2b5338bfe2ab1dba31861cdc8606e309ab18b
zhangyinhao <zhangyinhao1234@gmail.com>
2014-06-06 21:04:56 +0800

    33

--- ec/WebRoot/ec/js/utils/commons.js
+++
@@ -1,9 +0,0 @@
-var ec={};
-ec.js={};
-ec.js.utils={};
-ec.js.pagejs={};
-ec.js.utils.gotoLogin=function(){
-	(function(){
-		location.href='spring_security_login';
-	}).defer(2000)
-}
--- ec/src/com/org/zhang/context/EC.java
+++
@@ -1,8 +0,0 @@
-package com.org.zhang.context;
-
-public interface EC {
-	//系统全局变量
-	public interface Variable{
-		public final static String userRole="ROLE_USER" ;
-	}
-}
