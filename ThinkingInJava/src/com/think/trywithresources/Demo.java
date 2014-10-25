1521549c327f40354f3af58c5840970471a72fc8
zhangyinhao <zhangyinhao1234@gmail.com>
2014-10-25 22:01:27 +0800

    添加MD5测试

---
+++ ThinkingInJava/src/com/think/trywithresources/Demo.java
@@ -0,0 +1,23 @@
+package com.think.trywithresources;
+
+public class Demo {
+	private int age;
+	private String name;
+	public Demo(int age,String name){
+		this.age=age;
+		this.name=name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
