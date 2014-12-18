a2ab0894d0d0fca6af8daa6913c13763892c33fd
zhangyinhao <zhangyinhao1234@gmail.com>
2014-12-18 19:15:49 +0800

    测试ridis存储速度

--- RedisTest/src/com/bo/RedisTest1.java
+++ RedisTest/src/com/bo/RedisTest1.java
@@ -1,8 +1,14 @@
 package com.bo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
 import redis.clients.jedis.Jedis;
 
 import com.bean.UserInfo;
+import com.google.gson.Gson;
 import com.utils.DButil;
 import com.utils.SerializeUtil;
 
@@ -10,17 +16,36 @@ public class RedisTest1 {
 
 	public static void main(String[] args) {
 		Jedis jedis = DButil.getConnection();
-		UserInfo info  = new UserInfo();
-		info.setName("zhang");
-		info.setAge(22);
-		
-		jedis.set("mykey".getBytes(), SerializeUtil.serialize(info));
-		
-		
-		byte[] bs = jedis.get("mykey".getBytes());
-		UserInfo user =(UserInfo) SerializeUtil.unserialize(bs);
-		System.out.println(user.getAge());
-		
+		long t1 = System.currentTimeMillis();
+		List list=new ArrayList();
+		for(int i=0;i<100000;i++){
+			UserInfo info  = new UserInfo();
+			info.setName("zhang");
+			info.setAge(22);
+			list.add(info);
+			jedis.set(("mykey"+i).getBytes(), SerializeUtil.serialize(info));
+		}
+		long t2 = System.currentTimeMillis();
+		System.out.println(t2-t1); //22833
+	}
+	
+	
+	@Test
+	public void testSerialize(){
+		List list=new ArrayList();
+		for(int i=0;i<100000;i++){
+			UserInfo info  = new UserInfo();
+			info.setName("zhang");
+			info.setAge(22);
+			list.add(info);
+		}
+		byte[] serialize = SerializeUtil.serialize(list);
+		System.out.println(serialize.length);//84  1500123
+		Gson gson=new Gson();
+		byte[] bytes = gson.toJson(list).getBytes();
+		System.out.println(bytes.length);//25  2600001
+		byte[] serialize2 = SerializeUtil.serialize(gson.toJson(list));
+		System.out.println(serialize2.length);//32  2600014
 	}
 
 }
